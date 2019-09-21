6a284ed07b5aac1c99a200f6e182139fc631f8e4
reginaldo-lopes-dev <52450284+reginaldo-lopes-dev@users.noreply.github.com>
2019-09-21 00:16:15 -0300

    Calculadora feita através da linguagem C#
    No futuro, pretende-se adicionar casas decimais e outras funções úteis ( Como logaritmo e P.A e P.G).
    além é claro, de personaliza-la com a foto do shrek no background.

---
+++ interfacegrafica/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace interfacegrafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.csbtn0 = new System.Windows.Forms.Button();
+            this.cstb = new System.Windows.Forms.TextBox();
+            this.csbt8 = new System.Windows.Forms.Button();
+            this.csbt9 = new System.Windows.Forms.Button();
+            this.csbt7 = new System.Windows.Forms.Button();
+            this.csbt4 = new System.Windows.Forms.Button();
+            this.csbt5 = new System.Windows.Forms.Button();
+            this.csbt6 = new System.Windows.Forms.Button();
+            this.csbt1 = new System.Windows.Forms.Button();
+            this.csbt2 = new System.Windows.Forms.Button();
+            this.csbt3 = new System.Windows.Forms.Button();
+            this.csbtmais = new System.Windows.Forms.Button();
+            this.csbtmenos = new System.Windows.Forms.Button();
+            this.csbtmult = new System.Windows.Forms.Button();
+            this.csbtdiv = new System.Windows.Forms.Button();
+            this.csbtquad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.csbtigual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // csbtn0
+            // 
+            this.csbtn0.AccessibleName = "";
+            this.csbtn0.Location = new System.Drawing.Point(103, 327);
+            this.csbtn0.Name = "csbtn0";
+            this.csbtn0.Size = new System.Drawing.Size(75, 25);
+            this.csbtn0.TabIndex = 0;
+            this.csbtn0.Text = "0";
+            this.csbtn0.UseVisualStyleBackColor = true;
+            this.csbtn0.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // cstb
+            // 
+            this.cstb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cstb.Location = new System.Drawing.Point(103, 38);
+            this.cstb.Multiline = true;
+            this.cstb.Name = "cstb";
+            this.cstb.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cstb.Size = new System.Drawing.Size(182, 30);
+            this.cstb.TabIndex = 1;
+            this.cstb.TabStop = false;
+            this.cstb.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // csbt8
+            // 
+            this.csbt8.AccessibleName = "";
+            this.csbt8.Location = new System.Drawing.Point(103, 288);
+            this.csbt8.Name = "csbt8";
+            this.csbt8.Size = new System.Drawing.Size(75, 25);
+            this.csbt8.TabIndex = 2;
+            this.csbt8.Text = "8";
+            this.csbt8.UseVisualStyleBackColor = true;
+            this.csbt8.Click += new System.EventHandler(this.Csbt8_Click);
+            // 
+            // csbt9
+            // 
+            this.csbt9.AccessibleName = "";
+            this.csbt9.Location = new System.Drawing.Point(195, 288);
+            this.csbt9.Name = "csbt9";
+            this.csbt9.Size = new System.Drawing.Size(75, 25);
+            this.csbt9.TabIndex = 3;
+            this.csbt9.Text = "9";
+            this.csbt9.UseVisualStyleBackColor = true;
+            this.csbt9.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // csbt7
+            // 
+            this.csbt7.AccessibleName = "";
+            this.csbt7.Location = new System.Drawing.Point(12, 288);
+            this.csbt7.Name = "csbt7";
+            this.csbt7.Size = new System.Drawing.Size(75, 25);
+            this.csbt7.TabIndex = 4;
+            this.csbt7.Text = "7";
+            this.csbt7.UseVisualStyleBackColor = true;
+            this.csbt7.Click += new System.EventHandler(this.Csbt7_Click);
+            // 
+            // csbt4
+            // 
+            this.csbt4.AccessibleName = "";
+            this.csbt4.Location = new System.Drawing.Point(12, 238);
+            this.csbt4.Name = "csbt4";
+            this.csbt4.Size = new System.Drawing.Size(75, 25);
+            this.csbt4.TabIndex = 5;
+            this.csbt4.Text = "4";
+            this.csbt4.UseVisualStyleBackColor = true;
+            this.csbt4.Click += new System.EventHandler(this.Csbt4_Click);
+            // 
+            // csbt5
+            // 
+            this.csbt5.AccessibleName = "";
+            this.csbt5.Location = new System.Drawing.Point(103, 238);
+            this.csbt5.Name = "csbt5";
+            this.csbt5.Size = new System.Drawing.Size(75, 25);
+            this.csbt5.TabIndex = 6;
+            this.csbt5.Text = "5";
+            this.csbt5.UseVisualStyleBackColor = true;
+            this.csbt5.Click += new System.EventHandler(this.Csbt5_Click);
+            // 
+            // csbt6
+            // 
+            this.csbt6.AccessibleName = "";
+            this.csbt6.Location = new System.Drawing.Point(195, 238);
+            this.csbt6.Name = "csbt6";
+            this.csbt6.Size = new System.Drawing.Size(75, 25);
+            this.csbt6.TabIndex = 7;
+            this.csbt6.Text = "6";
+            this.csbt6.UseVisualStyleBackColor = true;
+            this.csbt6.Click += new System.EventHandler(this.Csbt6_Click);
+            // 
+            // csbt1
+            // 
+            this.csbt1.AccessibleName = "";
+            this.csbt1.Location = new System.Drawing.Point(12, 189);
+            this.csbt1.Name = "csbt1";
+            this.csbt1.Size = new System.Drawing.Size(75, 25);
+            this.csbt1.TabIndex = 8;
+            this.csbt1.Text = "1";
+            this.csbt1.UseVisualStyleBackColor = true;
+            this.csbt1.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // csbt2
+            // 
+            this.csbt2.AccessibleName = "";
+            this.csbt2.Location = new System.Drawing.Point(103, 189);
+            this.csbt2.Name = "csbt2";
+            this.csbt2.Size = new System.Drawing.Size(75, 25);
+            this.csbt2.TabIndex = 9;
+            this.csbt2.Text = "2";
+            this.csbt2.UseVisualStyleBackColor = true;
+            this.csbt2.Click += new System.EventHandler(this.Csbt2_Click);
+            // 
+            // csbt3
+            // 
+            this.csbt3.AccessibleName = "";
+            this.csbt3.Location = new System.Drawing.Point(195, 189);
+            this.csbt3.Name = "csbt3";
+            this.csbt3.Size = new System.Drawing.Size(75, 25);
+            this.csbt3.TabIndex = 10;
+            this.csbt3.Text = "3";
+            this.csbt3.UseVisualStyleBackColor = true;
+            this.csbt3.Click += new System.EventHandler(this.Csbt3_Click);
+            // 
+            // csbtmais
+            // 
+            this.csbtmais.AccessibleName = "";
+            this.csbtmais.Location = new System.Drawing.Point(12, 140);
+            this.csbtmais.Name = "csbtmais";
+            this.csbtmais.Size = new System.Drawing.Size(75, 25);
+            this.csbtmais.TabIndex = 11;
+            this.csbtmais.Text = "+";
+            this.csbtmais.UseVisualStyleBackColor = true;
+            this.csbtmais.Click += new System.EventHandler(this.Csbtmais_Click);
+            // 
+            // csbtmenos
+            // 
+            this.csbtmenos.AccessibleName = "";
+            this.csbtmenos.Location = new System.Drawing.Point(103, 140);
+            this.csbtmenos.Name = "csbtmenos";
+            this.csbtmenos.Size = new System.Drawing.Size(75, 25);
+            this.csbtmenos.TabIndex = 12;
+            this.csbtmenos.Text = "-";
+            this.csbtmenos.UseVisualStyleBackColor = true;
+            this.csbtmenos.Click += new System.EventHandler(this.Csbtmenos_Click);
+            // 
+            // csbtmult
+            // 
+            this.csbtmult.AccessibleName = "";
+            this.csbtmult.Location = new System.Drawing.Point(195, 140);
+            this.csbtmult.Name = "csbtmult";
+            this.csbtmult.Size = new System.Drawing.Size(75, 25);
+            this.csbtmult.TabIndex = 13;
+            this.csbtmult.Text = "X";
+            this.csbtmult.UseVisualStyleBackColor = true;
+            this.csbtmult.Click += new System.EventHandler(this.Csbtmult_Click);
+            // 
+            // csbtdiv
+            // 
+            this.csbtdiv.AccessibleName = "";
+            this.csbtdiv.Location = new System.Drawing.Point(195, 327);
+            this.csbtdiv.Name = "csbtdiv";
+            this.csbtdiv.Size = new System.Drawing.Size(75, 25);
+            this.csbtdiv.TabIndex = 14;
+            this.csbtdiv.Text = "/";
+            this.csbtdiv.UseVisualStyleBackColor = true;
+            this.csbtdiv.Click += new System.EventHandler(this.Csbtdiv_Click);
+            // 
+            // csbtquad
+            // 
+            this.csbtquad.AccessibleName = "";
+            this.csbtquad.Location = new System.Drawing.Point(12, 327);
+            this.csbtquad.Name = "csbtquad";
+            this.csbtquad.Size = new System.Drawing.Size(75, 25);
+            this.csbtquad.TabIndex = 15;
+            this.csbtquad.Text = "x²";
+            this.csbtquad.UseVisualStyleBackColor = true;
+            this.csbtquad.Click += new System.EventHandler(this.Csbtquad_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(215, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 18);
+            this.label1.TabIndex = 16;
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.Label1_Click_1);
+            // 
+            // csbtigual
+            // 
+            this.csbtigual.AccessibleName = "";
+            this.csbtigual.Location = new System.Drawing.Point(195, 109);
+            this.csbtigual.Name = "csbtigual";
+            this.csbtigual.Size = new System.Drawing.Size(75, 25);
+            this.csbtigual.TabIndex = 17;
+            this.csbtigual.Text = "=";
+            this.csbtigual.UseVisualStyleBackColor = true;
+            this.csbtigual.Click += new System.EventHandler(this.Csbtigual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 355);
+            this.Controls.Add(this.csbtigual);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.csbtquad);
+            this.Controls.Add(this.csbtdiv);
+            this.Controls.Add(this.csbtmult);
+            this.Controls.Add(this.csbtmenos);
+            this.Controls.Add(this.csbtmais);
+            this.Controls.Add(this.csbt3);
+            this.Controls.Add(this.csbt2);
+            this.Controls.Add(this.csbt1);
+            this.Controls.Add(this.csbt6);
+            this.Controls.Add(this.csbt5);
+            this.Controls.Add(this.csbt4);
+            this.Controls.Add(this.csbt7);
+            this.Controls.Add(this.csbt9);
+            this.Controls.Add(this.csbt8);
+            this.Controls.Add(this.cstb);
+            this.Controls.Add(this.csbtn0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button csbtn0;
+        private System.Windows.Forms.TextBox cstb;
+        private System.Windows.Forms.Button csbt8;
+        private System.Windows.Forms.Button csbt9;
+        private System.Windows.Forms.Button csbt7;
+        private System.Windows.Forms.Button csbt4;
+        private System.Windows.Forms.Button csbt5;
+        private System.Windows.Forms.Button csbt6;
+        private System.Windows.Forms.Button csbt1;
+        private System.Windows.Forms.Button csbt2;
+        private System.Windows.Forms.Button csbt3;
+        private System.Windows.Forms.Button csbtmais;
+        private System.Windows.Forms.Button csbtmenos;
+        private System.Windows.Forms.Button csbtmult;
+        private System.Windows.Forms.Button csbtdiv;
+        private System.Windows.Forms.Button csbtquad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button csbtigual;
+    }
+}
+
---
+++ interfacegrafica/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfacegrafica
+{
+    public partial class Form1 : Form
+    {
+        Double valor,valor1,valor2;
+        Double resultado;
+        int operacao;
+        public Form1() => InitializeComponent();
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            valor = 0;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            valor = 9;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            valor = 1;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Csbt2_Click(object sender, EventArgs e)
+        {
+            valor = 2;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbt3_Click(object sender, EventArgs e)
+        {
+            valor = 3;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbt4_Click(object sender, EventArgs e)
+        {
+            valor = 4;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbt5_Click(object sender, EventArgs e)
+        {
+            valor = 5;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbt6_Click(object sender, EventArgs e)
+        {
+            valor = 6;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbt7_Click(object sender, EventArgs e)
+        {
+            valor = 7;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbtmenos_Click(object sender, EventArgs e)
+        {
+            valor1 = Double.Parse(cstb.Text);
+            operacao = 2;
+            cstb.Clear();
+        }
+
+        private void Csbtmult_Click(object sender, EventArgs e)
+        {
+            valor1 = Double.Parse(cstb.Text);
+            operacao = 3;
+            cstb.Clear();
+        }
+
+        private void Csbtdiv_Click(object sender, EventArgs e)
+        {
+            valor1 = Double.Parse(cstb.Text);
+            operacao = 4;
+            cstb.Clear();
+        }
+
+        private void Csbtquad_Click(object sender, EventArgs e)
+        {
+            valor1 = Double.Parse(cstb.Text);
+            operacao = 5;
+            cstb.Clear();
+            resultado = valor1 * valor1;
+            cstb.Text = resultado.ToString();
+        }
+
+        private void Csbtigual_Click(object sender, EventArgs e)
+        {
+            if (operacao == 5)
+            {
+                resultado = valor1 * valor1;
+                cstb.Text = resultado.ToString();
+            }
+            else {
+                valor2 = Double.Parse(cstb.Text);
+                if (operacao == 1)
+                {
+                    resultado = valor1 + valor2;
+                    cstb.Text = resultado.ToString();
+                } else if (operacao == 2)
+                {
+                    resultado = valor1 - valor2;
+                    cstb.Text = resultado.ToString();
+                }
+                else if (operacao == 3)
+                {
+                    resultado = valor1 * valor2;
+                    cstb.Text = resultado.ToString();
+                }
+                else if (operacao == 4)
+                {
+                    resultado = valor1 / valor2;
+                    cstb.Text = resultado.ToString();
+                }
+            }
+        }
+
+        private void Csbt8_Click(object sender, EventArgs e)
+        {
+            valor = 8;
+            cstb.Text = cstb.Text + valor.ToString();
+        }
+
+        private void Csbtmais_Click(object sender, EventArgs e)
+        {
+            valor1 = Double.Parse(cstb.Text);
+            operacao = 1;
+            cstb.Clear();
+        }
+    }
+}
